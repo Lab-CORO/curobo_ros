92ab04ae7cd4d63f72b209f784212c5289c6df18
LuCarpentier92 <lucas.carpentier.1@ens.etsmtl.ca>
2024-08-15 16:17:38 -0400

    last commit

--- curobo_ros/curobo_test.py
+++ curobo_ros/curobo_test.py
@@ -42,39 +42,90 @@ class CuRoboTrajectoryMaker(Node):
         self.intrinsics = None
         self.cv_image = None
         self.depth_image = None
+        self.marker_data = None
         self.depth = 0
 
         #checker
         self.camera_info_received = False
         self.depth_image_received = False
+        self.marker_received = False
 
         self.bridge = CvBridge()
 
-        qos_profile = QoSProfile(
-            reliability=ReliabilityPolicy.RELIABLE,
-            history=HistoryPolicy.KEEP_LAST,
-            depth=10
+
+        self.world_cfg = WorldConfig.from_dict(
+
+            {
+                "blox": {
+                    "world": {
+                        "pose": [0, 0, 0, 1, 0, 0, 0],
+                        "integrator_type": "occupancy",
+                        "voxel_size": 0.02,
+                    },
+                },
+            }
         )
 
-        # self.subscription = self.create_subscription(
-        #     Image,
-        #     '/camera/camera/depth/image_rect_raw',
-        #     self.callback_depth,
-        #     qos_profile
-        # )
-        self.success, camera_info_sub = wait_for_message(CameraInfo, self, '/camera/camera/color/camera_info')
+        self.tensor_args = TensorDeviceType()
 
+        config_file_path = os.path.abspath(os.path.join(f"/home/ros2_ws/src/curobo_ros/m1013/m1013.yml"))
+        self.robot_cfg = load_yaml(config_file_path)["robot_cfg"]
+        self.j_names = self.robot_cfg["kinematics"]["cspace"]["joint_names"]
+        self.default_config = self.robot_cfg["kinematics"]["cspace"]["retract_config"]
+
+        world_cfg_table = WorldConfig.from_dict(load_yaml(join_path(get_world_configs_path(), "collision_wall.yml")))
+
+        world_cfg_table.cuboid[0].pose[2] -= 0.01
+        self.world_cfg.add_obstacle(world_cfg_table.cuboid[0])
+        self.world_cfg.add_obstacle(world_cfg_table.cuboid[1])
+
+        # robot_file = "franka.yml"
+        motion_gen_config = MotionGenConfig.load_from_robot_config(
+            self.robot_cfg,
+            self.world_cfg,
+            self.tensor_args,
+            trajopt_tsteps=32,
+            collision_checker_type=CollisionCheckerType.BLOX,
+            use_cuda_graph=True,
+            num_trajopt_seeds=12,
+            num_graph_seeds=12,
+            interpolation_dt=0.03,
+            collision_activation_distance=0.025,
+            acceleration_scale=1.0,
+            self_collision_check=True,
+            maximum_trajectory_dt=0.25,
+            finetune_dt_scale=1.05,
+            fixed_iters_trajopt=True,
+            finetune_trajopt_iters=300,
+            minimize_jerk=True,
+        )
+
+        self.motion_gen = MotionGen(motion_gen_config)
+
+        print("warming up..")
+
+        self.motion_gen.warmup()
+
+        self.world_model = self.motion_gen.world_collision
+
+        self.marker_sub = self.create_subscription(PoseStamped, 'marker_pose', self.callback_marker, 10)
+
+
+    def callback_marker(self, msg):
+        self.get_logger().info(f"Message reçu sur marker_pose: {msg}")
+        self.marker_data = msg
+        self.marker_received = True  # Met le booléen à True lorsque le message est reçu
+
+        #### CAMERA INFO ####
+        self.success, camera_info_sub = wait_for_message(CameraInfo, self, '/camera/camera/color/camera_info')
         if self.success:
             self.callback_camera_info(camera_info_sub)
-
+        #### DEPTH MAP ####
         self.success1, camera_depth_sub = wait_for_message(Image, self, '/camera/camera/depth/image_rect_raw')
         if self.success1:
             self.callback_depth(camera_depth_sub)
 
-        self.get_logger().info('Subscription to /camera/camera/depth/image_rect_raw created')
-
-
-
+        #### CUROBO FK
         self.client = self.create_client(Fk, '/curobo/fk_poses')
 
         while not self.client.wait_for_service(timeout_sec=1.0):
@@ -96,7 +147,6 @@ class CuRoboTrajectoryMaker(Node):
             self.future.add_done_callback(self.callback)
 
             self.marker_publisher = MarkerPublisher()
-
 
     def callback(self, future):
         try:
@@ -129,96 +179,15 @@ class CuRoboTrajectoryMaker(Node):
         except CvBridgeError as e:
             self.get_logger().error(f"An error has occurred: {e}")
 
-    # def check_and_generate_trajectory(self):
-    #     if self.camera_info_received == True and self.depth_image_received == True:
-    #         self.get_logger().info('Camera info and depth image received, generating trajectory...')
-    #         positions = self.trajectory_generator()
-    #
-    #         for i, element in enumerate(positions):
-    #             self.req.joint_states.append(SensorJointState())
-    #             self.req.joint_states[i].position = element
-    #             self.req.joint_states[i].name = ['joint1', 'joint2', 'joint3', 'joint4', 'joint5', 'joint6']
-    #
-    #         self.future = self.client.call_async(self.req)
-    #         self.future.add_done_callback(self.callback)
-
 
     def trajectory_generator(self):
 
-        radius = 0.05
-        clipping_distance = 0.5
-        act_distance = 0.4
-
-        voxel_size = 0.05
-        render_voxel_size = 0.02
-
-        tensor_args = TensorDeviceType()
-
-        collision_checker_type = CollisionCheckerType.BLOX
-
-        world_cfg = WorldConfig.from_dict(
-
-            {
-                "blox": {
-                    "world": {
-                        "pose": [0, 0, 0, 1, 0, 0, 0],
-                        "integrator_type": "occupancy",
-                        "voxel_size": 0.02,
-                    },
-                },
-            }
-        )
-
-        tensor_args = TensorDeviceType()
-
-        config_file_path = os.path.abspath(os.path.join(f"/home/ros2_ws/src/curobo_ros/m1013/m1013.yml"))
-        self.robot_cfg = load_yaml(config_file_path)["robot_cfg"]
-        self.j_names = self.robot_cfg["kinematics"]["cspace"]["joint_names"]
-        self.default_config = self.robot_cfg["kinematics"]["cspace"]["retract_config"]
-
-        world_cfg_table = WorldConfig.from_dict(load_yaml(join_path(get_world_configs_path(), "collision_wall.yml")))
-
-        world_cfg_table.cuboid[0].pose[2] -= 0.01
-        world_cfg.add_obstacle(world_cfg_table.cuboid[0])
-        world_cfg.add_obstacle(world_cfg_table.cuboid[1])
-
-        # robot_file = "franka.yml"
-        motion_gen_config = MotionGenConfig.load_from_robot_config(
-            self.robot_cfg,
-            world_cfg,
-            tensor_args,
-            trajopt_tsteps=32,
-            collision_checker_type=CollisionCheckerType.BLOX,
-            use_cuda_graph=True,
-            num_trajopt_seeds=12,
-            num_graph_seeds=12,
-            interpolation_dt=0.03,
-            collision_activation_distance=0.025,
-            acceleration_scale=1.0,
-            self_collision_check=True,
-            maximum_trajectory_dt=0.25,
-            finetune_dt_scale=1.05,
-            fixed_iters_trajopt=True,
-            finetune_trajopt_iters=300,
-            minimize_jerk=True,
-        )
-
-        motion_gen = MotionGen(motion_gen_config)
-
-        print("warming up..")
-
-        motion_gen.warmup()
-
-        world_model = motion_gen.world_collision
-
         ##################################################################################
         ##################################################################################
         ##################################################################################
         ##################################################################################
 
-        camera_pose = Pose.from_list([0.5, 1.0, 0.5, -0.44, 0.0, 0.88, -0.106])
-
-        # self.depth_sub = self.create_subscription(Image, "/camera/camera/color/image_rect_raw", self.callback_depth, 10)
+        camera_pose = Pose.from_list([0.5, 1.0, 0.5, -0.422551, 0.878444, 0.138406, 0.175016])
 
         self.get_logger().warning(f'depth image is {self.depth_image}')
 
@@ -228,10 +197,10 @@ class CuRoboTrajectoryMaker(Node):
         self.get_logger().warning(f" Intrinsics are : {data_camera.intrinsics}")
         self.get_logger().warning(f" Depth image is : {data_camera.depth_image}")
 
-        data_camera = data_camera.to(device=tensor_args.device)
-        world_model.add_camera_frame(data_camera, "world")
-        world_model.process_camera_frames("world", False)
-## a faire des qu un message est pub sur le topic du goal pose
+        data_camera = data_camera.to(device=self.tensor_args.device)
+        self.world_model.add_camera_frame(data_camera, "world")
+        self.world_model.process_camera_frames("world", False)
+        ## a faire des qu un message est pub sur le topic du goal pose
 
         ##################################################################################
         ##################################################################################
@@ -239,10 +208,12 @@ class CuRoboTrajectoryMaker(Node):
         ##################################################################################
 
         torch.cuda.synchronize()
-        world_model.update_blox_hashes()
+        self.world_model.update_blox_hashes()
 
         bounding = Cuboid("t", dims=[1, 1, 1.0], pose=[0, 0, 0, 1, 0, 0, 0])
-        voxels = world_model.get_voxels_in_bounding_box(bounding, voxel_size)
+        voxel_size = 0.05
+
+        voxels = self.world_model.get_voxels_in_bounding_box(bounding, voxel_size)
 
         if voxels.shape[0] > 0:
             voxels = voxels[voxels[:, 2] > voxel_size]
@@ -250,12 +221,8 @@ class CuRoboTrajectoryMaker(Node):
 
             voxels = voxels.cpu().numpy()
 
-        # motion_gen.warmup(enable_graph=True, warmup_js_trajopt=js, parallel_finetune=True)
-        # robot_cfg = load_yaml(join_path(get_robot_configs_path(), robot_file))["robot_cfg"]
-        # robot_cfg = RobotConfig.from_dict(robot_cfg, tensor_args)
-
-        retract_cfg = motion_gen.get_retract_config()
-        state = motion_gen.rollout_fn.compute_kinematics(
+        retract_cfg = self.motion_gen.get_retract_config()
+        state = self.motion_gen.rollout_fn.compute_kinematics(
             JointState.from_position(retract_cfg.view(1, -1))
         )
 
@@ -263,9 +230,8 @@ class CuRoboTrajectoryMaker(Node):
         start_state = JointState.from_position(retract_cfg.view(1, -1))
 
         self.get_logger().warn(f"Waiting for a pose to be published")
-        pose_msg = wait_for_message(PoseStamped, self, "/marker_pose")
+        pose_msg = self.marker_data
 
-        pose_msg = pose_msg[1]
         goal_pose = Pose.from_list([
             pose_msg.pose.position.x, pose_msg.pose.position.y, pose_msg.pose.position.z,
             pose_msg.pose.orientation.x, pose_msg.pose.orientation.y,
@@ -275,26 +241,27 @@ class CuRoboTrajectoryMaker(Node):
         self.get_logger().warn(f"Goal pose is {goal_pose}")
 
         start_state.position[0, 0] += 0.25
+        try:
+            result = self.motion_gen.plan_single(
+                start_state,
+                goal_pose,
+                MotionGenPlanConfig(
+                    max_attempts=1,
+                    timeout=5,
+                    time_dilation_factor=0.5,
+                ),
+            )
 
-        result = motion_gen.plan_single(
-            start_state,
-            goal_pose,
-            MotionGenPlanConfig(
-                max_attempts=1,
-                timeout=5,
-                time_dilation_factor=0.5,
-            ),
-        )
+            new_result = result.clone()
+            new_result.retime_trajectory(0.5, create_interpolation_buffer=True)
 
-        new_result = result.clone()
-        new_result.retime_trajectory(0.5, create_interpolation_buffer=True)
+            traj = result.get_interpolated_plan()
 
-        traj = result.get_interpolated_plan()
+            position = traj.position.cpu().tolist()
 
-        position = traj.position.cpu().tolist()
-
-        self.get_logger().warning(f'Positions are {position}')
-
+            self.get_logger().warning(f'Positions are {position}')
+        except:
+            position = []
         return position
 
 
