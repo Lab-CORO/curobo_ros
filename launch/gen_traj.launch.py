d9ab5bce9bb5a9541064c9943a3b23eda9fdfade
will-44 <guillaume.dupoiron@gmail.com>
2024-09-02 17:15:02 -0700

    create launch file but not finish

---
+++ launch/gen_traj.launch.py
@@ -0,0 +1,59 @@
+from launch import LaunchDescription
+from launch.actions import DeclareLaunchArgument, LogInfo
+from launch_ros.actions import Node
+from launch_ros.actions import PushRosNamespace
+from launch.actions import IncludeLaunchDescription
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+
+def generate_launch_description():
+    return LaunchDescription([
+        # Define the static transform publisher node
+        Node(
+            package='tf2_ros',
+            executable='static_transform_publisher',
+            name='static_transform_publisher',
+            output='screen',
+            arguments=['0.5', '0', '0.5', '0', '0', '0', 'base_0', 'camera_link']
+        ),
+
+        # Include the RViz2 launch file
+        IncludeLaunchDescription(
+            PythonLaunchDescriptionSource(['package://curobo_ros/launch/launch_rviz2.launch.py']),
+            launch_arguments={}.items()
+        ),
+
+        # Include the RealSense camera launch file
+        IncludeLaunchDescription(
+            PythonLaunchDescriptionSource(['package://realsense2_camera/launch/rs_launch.py']),
+            launch_arguments={'clip_distance': '0.8'}.items()
+        ),
+
+        # Run curobo_fk node
+        Node(
+            package='curobo_ros',
+            executable='curobo_fk',
+            name='curobo_fk',
+            output='screen'
+        ),
+        
+        # Run curobo_gen_traj node
+        Node(
+            package='curobo_ros',
+            executable='curobo_gen_traj',
+            name='curobo_gen_traj',
+            output='screen'
+        ),
+        
+        # Run curobo_int_mark node
+        Node(
+            package='curobo_ros',
+            executable='curobo_int_mark',
+            name='curobo_int_mark',
+            output='screen'
+        ),
+        
+        # Log an informational message
+        LogInfo(
+            msg='All nodes and launch files are launched'
+        ),
+    ])
