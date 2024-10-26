9ff76d907a00a546a9c6eed713ecf4dd01e74219
saxtot <saxtot@gmail.com>
2024-10-26 17:50:04 -0400

    Make motion_gen_config setup a fonction and a service

--- curobo_ros/core/generate_trajectory.py
+++ curobo_ros/core/generate_trajectory.py
@@ -12,6 +12,7 @@ from builtin_interfaces.msg import Duration
 from geometry_msgs.msg import PoseStamped
 from sensor_msgs.msg import JointState as SensorJointState
 from sensor_msgs.msg import Image, CameraInfo
+from std_srvs.srv import Trigger
 from trajectory_msgs.msg import JointTrajectory, JointTrajectoryPoint
 
 from curobo.geom.sdf.world import CollisionCheckerType
@@ -49,11 +50,13 @@ class CuRoboTrajectoryMaker(Node):
         self.depth_image = None
         self.marker_data = None
         self.depth = 0
-        self.voxel_size = 0.02
 
         self.marker_publisher = MarkerPublisher()
         self.trajectory_publisher = self.create_publisher(
             JointTrajectory, 'trajectory', 10)
+
+        self.motion_gen_srv = self.create_service(
+            Trigger, 'update_motion_gen_config', self.set_motion_gen_config)
 
         # checker
         self.depth_image_received = False
@@ -61,63 +64,28 @@ class CuRoboTrajectoryMaker(Node):
 
         self.bridge = CvBridge()
 
-        self.world_cfg = WorldConfig.from_dict(
-
-            {
-                "blox": {
-                    "world": {
-                        "pose": [0, 0, 0, 1, 0, 0, 0],
-                        "integrator_type": "occupancy",
-                        "voxel_size":  self.get_parameter('voxel_size').get_parameter_value().double_value,
-                    },
-                },
-            }
-        )
+        # World config parameters
+        self.world_pose = [0, 0, 0, 1, 0, 0, 0]
+        self.world_integrator_type = "occupancy"
 
         self.tensor_args = TensorDeviceType()
 
-        config_file_path = os.path.join(get_package_share_directory(
-            "curobo_ros"), 'curobo_doosan/src/m1013/m1013.yml')
-        self.robot_cfg = load_yaml(config_file_path)["robot_cfg"]
-        self.j_names = self.robot_cfg["kinematics"]["cspace"]["joint_names"]
-        self.default_config = self.robot_cfg["kinematics"]["cspace"]["retract_config"]
+        # Motion generation parameters
+        self.trajopt_tsteps = 32
+        self.collision_checker_type = CollisionCheckerType.BLOX
+        self.use_cuda_graph = True
+        self.num_trajopt_seeds = 12
+        self.num_graph_seeds = 12
+        self.interpolation_dt = 0.03
+        self.acceleration_scale = 1.0
+        self.self_collision_check = True
+        self.maximum_trajectory_dt = 0.25
+        self.finetune_dt_scale = 1.05
+        self.fixed_iters_trajopt = True
+        self.finetune_trajopt_iters = 300
+        self.minimize_jerk = True
 
-        world_cfg_table = WorldConfig.from_dict(
-            load_yaml(join_path(get_world_configs_path(), "collision_wall.yml")))
-
-        world_cfg_table.cuboid[0].pose[2] -= 0.01
-        self.world_cfg.add_obstacle(world_cfg_table.cuboid[0])
-        self.world_cfg.add_obstacle(world_cfg_table.cuboid[1])
-
-        motion_gen_config = MotionGenConfig.load_from_robot_config(
-            self.robot_cfg,
-            self.world_cfg,
-            self.tensor_args,
-            trajopt_tsteps=32,
-            collision_checker_type=CollisionCheckerType.BLOX,
-            use_cuda_graph=True,
-            num_trajopt_seeds=12,
-            num_graph_seeds=12,
-            interpolation_dt=0.03,
-            collision_activation_distance=self.get_parameter(
-                'collision_activation_distance').get_parameter_value().double_value,
-            acceleration_scale=1.0,
-            self_collision_check=True,
-            maximum_trajectory_dt=0.25,
-            finetune_dt_scale=1.05,
-            fixed_iters_trajopt=True,
-            finetune_trajopt_iters=300,
-            minimize_jerk=True,
-        )
-        self.tensor_args = TensorDeviceType()
-
-        self.motion_gen = MotionGen(motion_gen_config)
-
-        self.get_logger().info("warming up..")
-
-        self.motion_gen.warmup()
-
-        self.world_model = self.motion_gen.world_collision
+        self.set_motion_gen_config(None, None)
 
         self.marker_sub = self.create_subscription(
             PoseStamped, 'marker_pose', self.callback_marker, 1)
@@ -145,6 +113,63 @@ class CuRoboTrajectoryMaker(Node):
         # create time for traj gen
         timer_period = 0.1  # seconds
         self.timer = self.create_timer(timer_period, self.trajectory_generator)
+
+    def set_motion_gen_config(self, request, response):
+        self.world_cfg = WorldConfig.from_dict(
+            {
+                "blox": {
+                    "world": {
+                        "pose": self.world_pose,
+                        "integrator_type": self.world_integrator_type,
+                        "voxel_size":  self.get_parameter('voxel_size').get_parameter_value().double_value,
+                    },
+                },
+            }
+        )
+
+        config_file_path = os.path.join(get_package_share_directory(
+            "curobo_ros"), 'curobo_doosan/src/m1013/m1013.yml')
+        self.robot_cfg = load_yaml(config_file_path)["robot_cfg"]
+        self.j_names = self.robot_cfg["kinematics"]["cspace"]["joint_names"]
+        self.default_config = self.robot_cfg["kinematics"]["cspace"]["retract_config"]
+
+        self.world_cfg_table = WorldConfig.from_dict(
+            load_yaml(join_path(get_world_configs_path(), "collision_wall.yml")))
+
+        self.world_cfg_table.cuboid[0].pose[2] -= 0.01
+        self.world_cfg.add_obstacle(self.world_cfg_table.cuboid[0])
+        self.world_cfg.add_obstacle(self.world_cfg_table.cuboid[1])
+
+        self.motion_gen_config = MotionGenConfig.load_from_robot_config(
+            self.robot_cfg,
+            self.world_cfg,
+            self.tensor_args,
+            trajopt_tsteps=self.trajopt_tsteps,
+            collision_checker_type=self.collision_checker_type,
+            use_cuda_graph=self.use_cuda_graph,
+            num_trajopt_seeds=self.num_trajopt_seeds,
+            num_graph_seeds=self.num_graph_seeds,
+            interpolation_dt=self.interpolation_dt,
+            collision_activation_distance=self.get_parameter(
+                'collision_activation_distance').get_parameter_value().double_value,
+            acceleration_scale=self.acceleration_scale,
+            self_collision_check=self.self_collision_check,
+            maximum_trajectory_dt=self.maximum_trajectory_dt,
+            finetune_dt_scale=self.finetune_dt_scale,
+            fixed_iters_trajopt=self.fixed_iters_trajopt,
+            finetune_trajopt_iters=self.finetune_trajopt_iters,
+            minimize_jerk=self.minimize_jerk,
+        )
+
+        self.motion_gen = MotionGen(self.motion_gen_config)
+
+        self.get_logger().info("warming up..")
+
+        self.motion_gen.warmup()
+
+        self.world_model = self.motion_gen.world_collision
+
+        self.get_logger().info("Motion generation config set")
 
     def callback_depth(self, msg):
         try:
@@ -242,12 +267,15 @@ class CuRoboTrajectoryMaker(Node):
         # Voxel debug
         bounding = Cuboid("t", dims=[1, 1, 1.0], pose=[1, 0, 0.5, 1, 0, 0, 0])
 
+        voxel_size = self.get_parameter(
+            'voxel_size').get_parameter_value().double_value
+
         voxels = self.world_model.get_voxels_in_bounding_box(
-            bounding, self.voxel_size)
-        # print(len(voxels))
+            bounding, voxel_size)
+
         if voxels.shape[0] > 0:
             voxels = voxels.cpu().numpy()
-        self.marker_publisher.publish_markers_voxel(voxels, self.voxel_size)
+        self.marker_publisher.publish_markers_voxel(voxels, voxel_size)
 
     def trajectory_generator(self):
         if not self.marker_received:
