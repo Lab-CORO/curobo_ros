f6d9aadf64b5e476efbb8b9fb238eb1fd4a6f045
KarynaFL <karyna-f.ladouceur.1@ens.etsmtl.ca>
2024-12-03 14:30:44 -0500

    add entry point robot_segmentation

--- setup.py
+++ setup.py
@@ -33,6 +33,7 @@ setup(
             'curobo_fk = curobo_ros.core.fk:main',
             'curobo_gen_traj = curobo_ros.core.generate_trajectory:main',
             'curobo_int_mark = curobo_ros.interfaces.simple_arrow:main',
+            'robot_segmentation = curobo_ros.core.robot_segmentation:main',
             # 'test_fk = curobo_ros.test_fk:main'
         ],
     },
