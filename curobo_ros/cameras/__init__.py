0dd47668d785b8d7c60c45ec39fef7ee67c15c82
will-44 <guillaume.dupoiron@protonmail.com>
2025-11-05 19:13:14 -0500

    change names

--- curobo_ros/cameras/__init__.py
+++ curobo_ros/cameras/__init__.py
@@ -3,7 +3,7 @@
 from curobo_ros.cameras.camera_strategy import CameraStrategy
 from curobo_ros.cameras.camera_context import CameraContext
 from curobo_ros.cameras.pointcloud_camera_strategy import PointCloudCameraStrategy
-from curobo_ros.cameras.camera_simulation_strategy import RealsenseStrategy
+from curobo_ros.cameras.camera_depth_map_strategy import DepthMapCameraStrategy
 
 __all__ = [
     'CameraStrategy',
